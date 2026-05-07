65abf9a4b051b236784b28b266b723fa3c571a16
alyssonxoliveira0-dotcom <alyssonxoliveira0@gmail.com>
2026-05-07 01:51:35 -0300

    fix: remove storage edge function, usa Node.js function sem imports

--- netlify/edge-functions/storage.ts
+++
@@ -1,6 +0,0 @@
-// Stub: storage moved to netlify/functions/storage.js (Node.js, @netlify/blobs pre-bundled)
-export default async (request: Request) => {
-  const url = new URL(request.url);
-  const target = new URL('/.netlify/functions/storage' + url.search, url.origin);
-  return Response.redirect(target.toString(), 307);
-};
